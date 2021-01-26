dfe09905764e5c4946ef493727641cf8e8e0396c
jhalkola <joona.halkola@hotmail.com>
2021-01-26 21:07:04 +0200

    Created main feed design

--- app/src/main/java/com/example/reminderapp/LoginActivity.kt
+++ app/src/main/java/com/example/reminderapp/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.reminderapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.buttonLogin)
+        loginButton.setOnClickListener {
+            var mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
     }
 }
--- app/src/main/java/com/example/reminderapp/MainActivity.kt
+++ app/src/main/java/com/example/reminderapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.reminderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "Virat Kohli", "Rohit Sharma", "Steve Smith",
+            "Kane Williamson", "Ross Taylor"
+        )
+
+        listView = findViewById<ListView>(R.id.listViewMain)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+        listView.adapter = arrayAdapter
     }
 }
